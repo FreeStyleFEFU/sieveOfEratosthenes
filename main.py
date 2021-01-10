0672c451ef5a5b78e2a39efcc8c876d23630f013
Михаил Гусев <gusev-miha2011@mail.ru>
2021-01-10 22:35:51 +1000

    Add Monitoring processes Add continue processes after closes program

--- main.py
+++ main.py
@@ -1,54 +1,101 @@
 import concurrent.futures
-import multiprocessing
-from multiprocessing import Pipe
+from multiprocessing import Process, Queue
+import multiprocessing as mp
 import time
+
+sentinel = -1
 
 def algoritm(i, numbers, result, q):
     for j in range(i+1, len(numbers)-i):
-        q.put(result[i+j])
+        q.put([numbers[i], numbers[i+j], len(numbers)])
         if numbers[i+j] % numbers[i] == 0:
             result[i+j] = 1
+
+    while q.empty() != True:
+        q.get()
     return result
 
-def progress(q, i, numbers):
-        print(f'Вычисление для числа {numbers[i]}: {q.get()} из {len(numbers)}')
+def progress(q):
+    while True:
+        time.sleep(1)
+        data = q.get()
+        print(f'Вычисление для числа {data[0]}: {data[1]} из {data[2]}')
 
+        if data is sentinel:
+            break
 
-if __name__ == '__main__':
+def gui():
+    last = 0
+    simple = []
+
+    print("1 - продолжить вычисления; 2 - новые вычисления")
+
+    choice = input()
+    try:
+        int(choice)
+    except:
+        print("Ошибка ввода данных")
+        gui()
+
+    choice = int(choice)
+    if choice != 1 and choice != 2:
+        print("Нет такого выбора")
+        gui()
+
     print("До какого числа искать?")
     quantity = int(input())
 
     numbers = [i for i in range(2, quantity)]
     flags = [0 for i in range(2, quantity)]
 
-    simple = []
+    if choice == 1:
+        with open("simple.txt", "r", encoding='utf-8') as file:
+            contents = file.readlines()
+            for line in contents:
+                simple.append(int(line))
+        with open("flags.txt", "r", encoding='utf-8') as file:
+            contents = file.readlines()
+            for flag in range(len(contents)):
+                flags[flag] = int(contents[flag])
+        last = simple[-1]-1
 
-    i = 0
+    elif choice == 2:
+        with open("simple.txt", "w", encoding='utf-8') as file:
+            file.writelines("")
+        with open("flags.txt", "w", encoding='utf-8') as file:
+            file.writelines("")
+
+    m = mp.Manager()
+
+    q1 = m.Queue()
+    q2 = m.Queue()
+
+    monitor1 = Process(target=progress, args=(q1,))
+    monitor2 = Process(target=progress, args=(q2,))
+    monitor1.start()
+    monitor2.start()
+
+    print(last, flags)
+
+    i = last
     while i < len(numbers):
         if flags[i] == 0:
             simple.append(numbers[i])
-            with open("simple.txt", "w", encoding='utf-8') as file:
-                for number in simple:
-                    file.writelines(f"{number}\n")
+            with open("simple.txt", "a", encoding='utf-8') as file:
+                    file.writelines(f"{simple[-1]}\n")
             j = i + 1
             while j < len(numbers):
                 if flags[j] == 0:
                     simple.append(numbers[j])
-                    with open("simple.txt", "w", encoding='utf-8') as file:
-                        for number in simple:
-                            file.writelines(f"{number}\n")
+                    with open("simple.txt", "a", encoding='utf-8') as file:
+                            file.writelines(f"{simple[-1]}\n")
                     resultProcesses = []
                     resultProcess1 = [0 for i in range(2, quantity)]
                     resultProcess2 = [0 for i in range(2, quantity)]
                     with concurrent.futures.ProcessPoolExecutor() as executor:
-                        progress1 = multiprocessing.Queue()
-                        progress2 = multiprocessing.Queue()
 
-                        resultProcesses.append(executor.submit(algoritm, i, numbers, resultProcess1, progress1))  #процессы с рассчетами
-                        resultProcesses.append(executor.submit(algoritm, j, numbers, resultProcess2, progress2))
-
-                        executor.submit(progress, progress1, i, numbers)   # процессы для мониторинга прогресса
-                        executor.submit(progress, progress2, j, numbers)
+                        resultProcesses.append(executor.submit(algoritm, i, numbers, resultProcess1, q1))  # процессы с рассчетами
+                        resultProcesses.append(executor.submit(algoritm, j, numbers, resultProcess2, q2))
 
                         concurrent.futures.as_completed(resultProcesses)
 
@@ -58,9 +105,15 @@ if __name__ == '__main__':
                     i += j - i
                     break
                 j += 1
+        with open("flags.txt", "w", encoding='utf-8') as flagsFile:
+            for flag in flags:
+                flagsFile.writelines(f"{flag}\n")
         i += 1
 
-    
+    monitor1.kill()
+    monitor2.kill()
 
+    print("Готово")
 
-
+if __name__ == '__main__':
+    gui()
